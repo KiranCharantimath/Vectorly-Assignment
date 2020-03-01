84e1dda4088289243af0bdbce3df90f311557fe1
kiran <kiranbc050@gmail.com>
2020-03-01 17:31:35 +0530

    Input Image parameter changed

--- get_text_overlay.py
+++ get_text_overlay.py
@@ -1,8 +1,8 @@
 import cv2
 import numpy as np
 
-def getTextOverlay(input_image):
-    output = np.zeros(input_image.shape, dtype=np.uint8)
+def getTextOverlay(ip):
+    output = np.zeros(ip.shape, dtype=np.uint8)
      
     # Write your code here for output
     output[np.where((input_image!=[0,0,0]).all(axis=2))] = [255,255,255] #Update all pixels which r not black to white
